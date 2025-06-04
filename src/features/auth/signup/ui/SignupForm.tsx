fd709e8b6c59249d4c4c1f79645471aae399d026
Joseph Park <rkfnahs12e@gmail.com>
2025-06-04 17:42:00 +0900

    refactor : 회원가입도 모달로 변경

--- src/features/auth/signup/ui/SignupForm.tsx
+++ src/features/auth/signup/ui/SignupForm.tsx
@@ -10,6 +10,7 @@ import { FormField, FormItem, FormMessage } from '@/shared/ui/form';
 import GenderPopover from '@/entities/auth/ui/GenderPopover';
 import CalendarPopover from '@/entities/auth/ui/CalendarPopover';
 import { SignUpFormType } from '@/entities/auth/model/types';
+import { BackButton } from '@/shared/ui/backButton';
 
 export function SignupForm() {
   const { mutate } = useSignup();
@@ -47,45 +48,49 @@ export function SignupForm() {
   };
 
   return (
-    <div className='flex flex-col items-center gap-4 justify-center '>
-      <h1 className='text-2xl font-bold mt-4'>회원가입</h1>
-      <AuthForm form={form} onSubmit={onSubmit} formType='signup'>
-        <AuthInput
-          form={form}
-          name='confirmPassword'
-          placeholder='비밀번호 확인'
-          type='password'
-        />
-        <AuthInput form={form} name='nickname' placeholder='닉네임' />
-        <AuthInput
-          form={form}
-          name='game_nickname'
-          placeholder='게임 닉네임(ex. player#KR1)'
-        />
-        <AuthInput form={form} name='phone' placeholder='전화번호' />
-        <div className='flex gap-4'>
-          <FormField
-            control={form.control}
-            name='gender'
-            render={({ field }) => (
-              <FormItem className='flex-1'>
-                <GenderPopover field={field} />
-                <FormMessage />
-              </FormItem>
-            )}
+    <div className='fixed inset-0 z-50 flex items-center justify-center bg-black/50 backdrop-blur-sm'>
+      <div className='relative flex flex-col items-center gap-4 justify-center bg-background rounded-xl shadow-2xl p-8 min-w-[340px]'>
+        <BackButton />
+
+        <h1 className='text-2xl font-bold mt-4'>회원가입</h1>
+        <AuthForm form={form} onSubmit={onSubmit} formType='signup'>
+          <AuthInput
+            form={form}
+            name='confirmPassword'
+            placeholder='비밀번호 확인'
+            type='password'
           />
-          <FormField
-            control={form.control}
-            name='birthday'
-            render={({ field }) => (
-              <FormItem className='flex-1'>
-                <CalendarPopover field={field} />
-                <FormMessage />
-              </FormItem>
-            )}
+          <AuthInput form={form} name='nickname' placeholder='닉네임' />
+          <AuthInput
+            form={form}
+            name='game_nickname'
+            placeholder='게임 닉네임(ex. player#KR1)'
           />
-        </div>
-      </AuthForm>
+          <AuthInput form={form} name='phone' placeholder='전화번호' />
+          <div className='flex gap-4'>
+            <FormField
+              control={form.control}
+              name='gender'
+              render={({ field }) => (
+                <FormItem className='flex-1'>
+                  <GenderPopover field={field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name='birthday'
+              render={({ field }) => (
+                <FormItem className='flex-1'>
+                  <CalendarPopover field={field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+        </AuthForm>
+      </div>
     </div>
   );
 }
