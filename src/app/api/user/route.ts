acf84c5459adcc113e8f41e28a046bbe07b25d91
Joseph Park <rkfnahs12e@gmail.com>
2025-06-10 17:39:25 +0900

    feat : 유저 프로필 이미지 수정 기능 추가

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -28,18 +28,37 @@ export async function PATCH(request: Request) {
   const game_nickname = formData.get('game_nickname');
   const tier = formData.get('tier');
   const one_line = formData.get('one_line');
-  console.log(userId, game_nickname, tier, one_line);
+  const profile_image = formData.get('profile_image');
+  const supabase = await createClient();
+  let profile_image_url: string = '';
+
   if (!userId) {
     return NextResponse.json({ error: 'userId is not found' }, { status: 400 });
   }
 
-  const supabase = await createClient();
+  if (profile_image) {
+    const ext = (profile_image as File).name.split('.').pop() || 'png';
+    const fileName = `${userId}.${ext}`;
+    const { error: uploadError } = await supabase.storage
+      .from('profile')
+      .upload(fileName, await (profile_image as File).arrayBuffer(), {
+        contentType: (profile_image as File).type,
+        upsert: true,
+      });
+    if (uploadError) {
+      return NextResponse.json({ error: uploadError.message }, { status: 500 });
+    }
+    const { data } = supabase.storage.from('profile').getPublicUrl(fileName);
+    profile_image_url = data.publicUrl;
+  }
+
   const { data: user, error } = await supabase
     .from('profiles')
     .update({
       game_nickname,
       tier,
       one_line,
+      profile_image: profile_image_url,
     })
     .eq('id', userId)
     .single();
