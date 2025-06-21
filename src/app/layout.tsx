78c66783f6636ad2be980c047b64a705912b36c2
Joseph Park <rkfnahs12e@gmail.com>
2025-06-21 19:47:36 +0900

    fix : vercel url env 이름 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import { ThemeProvider } from 'next-themes';
 import '@/app/globals.css';
 import { TanstackQueryProvider } from '@/shared/providers';
 
-const defaultUrl = process.env.VERCEL_URL
-  ? `https://${process.env.VERCEL_URL}`
+const defaultUrl = process.env.NEXT_PUBLIC_VERCEL_URL
+  ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
   : 'http://localhost:3000';
 
 export const metadata = {
--- src/shared/lib/utils.ts
+++ src/shared/lib/utils.ts
@@ -55,8 +55,8 @@ export async function fetcher<TResponse = unknown>(
   };
 
   const response = await fetch(
-    process.env.VERCEL_URL
-      ? `${process.env.VERCEL_URL}` + requestUrl
+    process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` + requestUrl
       : `${process.env.NEXT_PUBLIC_BASE_URL}` + requestUrl,
     fetchOptions
   );
